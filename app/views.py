927cc6f41093c7fcc0fc8fe9db68df0180ec9aff
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-27 00:51:37 +0300

    add getting popular and current news

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+from .request import get_news
+
+@app.route('/')
+def index():
+
+
+
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_news = get_news('popular')
+    upcoming_news = get_news('upcoming')
+    now_showing_news = get_news('now_playing')
+    title = 'Home - Welcome to The best news Review Website Online'
+    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news )
+    
